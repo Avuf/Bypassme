d01091f655a04d896cd9b42e95df19ec59bd3fff
Avuf <127880889+Avuf@users.noreply.github.com>
2023-08-16 09:10:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,5 +9,5 @@ DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 ADMINS = [
     int(admin) if id_pattern.search(admin) else admin
-    for admin in environ.get('ADMINS', '5274370570, add your id').split(',')
+    for admin in environ.get('ADMINS', '5274370570,5164928761').split(',')
 ]

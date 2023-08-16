ec58ddb6e88f9cf1188af062fabbcf7b2bda1ac4
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-16 18:56:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,6 @@
 import re
 from os import environ
+from database import db
 id_pattern = re.compile(r'^.\d+$')
 
 CHANNEL_ONE =int(environ.get("CHANNEL_ONE", "-1001983799712"))
@@ -15,3 +16,19 @@ ADMINS = [
 class temp(object):
     LINK_ONE = None
     LINK_TWO = None
+
+async def is_requested_one(self , message):
+    user = await db.get_req_one(int(message.from_user.id))
+    if user:
+        return True
+    if message.from_user.id in ADMINS:
+        return True
+    return False
+    
+async def is_requested_two(self, message):
+    user = await db.get_req_two(int(message.from_user.id))
+    if user:
+        return True
+    if message.from_user.id in ADMINS:
+        return True
+    return False

949dec685ab4251c8d8b175f6cd0a30b34008ec4
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-17 10:41:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,16 +15,16 @@ class temp(object):
     LINK_ONE = None
     LINK_TWO = None
 
-async def is_requested_one(self , message):
-    user = await db.get_req_one(int(message.from_user.id))
+def is_requested_one(self , message):
+    user = db.get_req_one(int(message.from_user.id))
     if user:
         return True
     if message.from_user.id in ADMINS:
         return True
     return False
     
-async def is_requested_two(self, message):
-    user = await db.get_req_two(int(message.from_user.id))
+def is_requested_two(self, message):
+    user = db.get_req_two(int(message.from_user.id))
     if user:
         return True
     if message.from_user.id in ADMINS:

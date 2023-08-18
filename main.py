bee728f88a9cab82e1b6ee4685be7071786176d8
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-08-18 16:59:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,5 @@
-import pyrogram
 from pyrogram import Client
-from pyrogram import filters, enums
-from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton, ChatJoinRequest, Message
-import bypasser
-import os
-import ddl
-import requests
-import threading 
-from texts import HELP_TEXT
-from bypasser import ispresent, SITES_TEXT, gdtot3, GDTot_Crypt
-import requests
-from requests import get as rget
-import base64
-from urllib.parse import unquote, urlparse, parse_qs, quote
-import time
-import cloudscraper
-from bs4 import BeautifulSoup, NavigableString, Tag
-from lxml import etree
-from cloudscraper import create_scraper
-from uuid import uuid4
-import hashlib
-import json
-from dotenv import load_dotenv
-load_dotenv()
-from asyncio import sleep as asleep
-import PyBypass
-import os
-from pyrogram import Client
-from pyrogram.types import Message 
-from cloudscraper import create_scraper
-from os import path
-from http.cookiejar import MozillaCookieJar
-from lxml import etree
-from re import findall, match, search, sub
-from uuid import uuid4
-import time
-import datetime
-from database import db
-from info import CHANNEL_TWO, CHANNEL_ONE, ADMINS, is_requested_one, is_requested_two, broadcast_messages
-from bypasser import *
-from pyrogram.enums import MessageEntityType, ChatMemberStatus
-from pyrogram.errors import RPCError, FloodWait, UserNotParticipant
+
 from aiohttp import web
 from plugins import web_server
 
@@ -70,7 +29,7 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("Bot stopped. Bye.")
+        print("Bot stopped. Bye.")
 
 
 app = Bot()
